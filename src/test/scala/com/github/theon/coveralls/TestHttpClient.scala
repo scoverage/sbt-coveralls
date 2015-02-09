9a62203092ab2c0a0080b76a6d6a56e569c7cbbb
Edouard Pelosi <edouard.pelosi@gmail.com>
2015-02-09 12:10:21 +0900

    tests created to provoke the error

--- src/test/scala/com/github/theon/coveralls/TestHttpClient.scala
+++ src/test/scala/com/github/theon/coveralls/TestHttpClient.scala
@@ -8,24 +8,30 @@ class TestSuccessHttpClient extends HttpClient {
   def multipart(url: String, name: String, filename: String, mime: String, data: Array[Byte]) = {
     dataIn = new String(data, "UTF-8")
     new CoverallHttpResponse(200,
-    """
+      """
       {
         "message":"test message",
         "error": false,
         "url": "https://github.com/theon/xsbt-coveralls-plugin"
       }
-    """)
+      """)
   }
 }
 
 class TestFailureHttpClient extends HttpClient {
   def multipart(url: String, name: String, filename: String, mime: String, data: Array[Byte]) = {
     new CoverallHttpResponse(200,
-    """
+      """
       {
         "message":"test error message when there was an error",
         "error": true
       }
-    """)
+      """)
+  }
+}
+
+case class FakeTestHttpClient(status: Int, body: String) extends HttpClient {
+  def multipart(url: String, name: String, filename: String, mime: String, data: Array[Byte]) = {
+    new CoverallHttpResponse(status, body)
   }
 }
