2835eb429ec973adddc7f8bcbe1350ef6e6744d5
Ryan Williams <ryan.blake.williams@gmail.com>
2017-08-07 20:15:45 +0000

    support calling from within a git submodule

--- src/main/scala/org/scoverage/coveralls/GitClient.scala
+++ src/main/scala/org/scoverage/coveralls/GitClient.scala
@@ -1,6 +1,7 @@
 package org.scoverage.coveralls
 
 import java.io.File
+import java.nio.file.Files.lines
 
 import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder
@@ -19,14 +20,38 @@ object GitClient {
 
 class GitClient(cwd: String)(implicit log: Logger) {
 
-  import scala.collection.JavaConversions._
+  import scala.collection.JavaConverters._
 
-  val repository = FileRepositoryBuilder.create(new File(cwd, ".git"))
+  val gitDirLineRegex = """^gitdir: (.*)""".r
+
+  val gitFile = new File(cwd, ".git")
+
+  val resolvedGitDir =
+    if (gitFile.isFile)
+      lines(gitFile.toPath)
+        .iterator()
+        .asScala
+        .toList match {
+          case gitDirLineRegex(dir) :: Nil ⇒
+            log.info(s"Resolved git submodule file $gitFile to $dir")
+            new File(dir)
+          case lines ⇒
+            throw new IllegalArgumentException(
+              s"Expected single 'gitdir' line in .git file, found:\n\t${lines.mkString("\n\t")}"
+            )
+        }
+    else
+      gitFile
+
+  val repository = FileRepositoryBuilder.create(resolvedGitDir)
   val storedConfig = repository.getConfig
   log.info("Repository = " + repository.getDirectory)
 
   def remotes: Seq[String] = {
-    storedConfig.getSubsections("remote").toList
+    storedConfig
+      .getSubsections("remote")
+      .asScala
+      .toList
   }
 
   def remoteUrl(remoteName: String): String = {
@@ -38,7 +63,7 @@ class GitClient(cwd: String)(implicit log: Logger) {
 
   def lastCommit(): GitRevision = {
     val git = new Git(repository)
-    val headRev = git.log().setMaxCount(1).call().head
+    val headRev = git.log().setMaxCount(1).call().asScala.head
     val id = headRev.getId
     val author = headRev.getAuthorIdent
     val committer = headRev.getCommitterIdent
