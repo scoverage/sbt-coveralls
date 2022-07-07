412f4cd79032ac394b26a62b13d015aacdf64d1a
Roland Tritsch <roland@tritsch.email>
2022-07-07 15:04:24 +0100

    Adding mkFileFromPath (to make sure Windows works too).

---
+++ src/main/scala/org/scoverage/coveralls/Utils.scala
@@ -0,0 +1,16 @@
+package org.scoverage.coveralls
+
+import java.io.File
+
+object Utils {
+  def mkFileFromPath(path: Seq[String]): File = {
+    require(path.nonEmpty, "path cannot be empty")
+    val (p :: ps) = path
+    mkFileFromPath(new File(p), ps)
+  }
+
+  def mkFileFromPath(base: File, path: Seq[String]): File = path match {
+    case p :: ps => mkFileFromPath(new File(base, p), ps)
+    case _ => base
+  }
+}
