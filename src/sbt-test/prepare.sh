6cb727a655a9ef0176261e7e6f5698e458215d66
Roland Tritsch <roland@tritsch.email>
2022-12-15 07:11:02 +0000

    Make sbt-test (sbt-scoverage-samples) work/pass.

--- src/sbt-test/prepare.sh
+++ src/sbt-test/prepare.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for submodule_path in $(git submodule status | cut -d\  -f3); do echo "gitdir: ${PWD}/.git/modules/${submodule_path}" > ${submodule_path}/.git; done
+for submodule_path in $(git submodule status | cut -c2- | cut -d\  -f2); do echo "gitdir: ${PWD}/.git/modules/${submodule_path}" > ${submodule_path}/.git; done
